20af1186a0623e47f5937cd3efe8756405124c89
Govardhan <139302088+Govardhan8378@users.noreply.github.com>
2024-07-27 11:01:57 +0530

    Add files via upload

---
+++ Alfido Tech/script.js
@@ -0,0 +1,25 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function myFunction() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch {
+        document.getElementById('display').value = 'Error';
+    }
+  }
